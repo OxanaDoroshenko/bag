163d340a8ad26cf0771642f094e8af4e46a7f75d
OxanaDoroshenko <donotsettlebebrave@gmail.com>
2018-03-25 18:22:03 +0400

    clean project from useless models

--- db/models/index.js
+++ db/models/index.js
@@ -1,10 +1,8 @@
 'use strict';
 
-const Product = require('./product')
-const Review = require('./review');
 const Company = require('./company');
 
-Product.hasMany(Review);
-Review.belongsTo(Product);
+// Product.hasMany(Review);
+// Review.belongsTo(Product);
 
-module.exports = {Product, Review, Company};
+module.exports = {Product};
